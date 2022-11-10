3c12f5c7bd4d5e7dfaab822603497e5b9514786b
Heldereim <adam.hellgard@hotmail.se>
2022-11-10 13:31:28 +0100

    Created Car class

---
+++ Car.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Car {
+    
+    private int nrDoors;
+    private double enginePower;
+    private double currentSpeed;
+    private Color color;
+    private String modelName;
+
+    public Car(){
+        
+    }
+}

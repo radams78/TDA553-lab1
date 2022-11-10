81e02db18e1e4bd49c3ce2a831e1914beaf5781f
Programmer64fortnite <84543653+Programmer64fortnite@users.noreply.github.com>
2022-11-10 14:40:15 +0100

    Revert "Update Saab95.java"
    This reverts commit 589a136bdfb17f4f818ff1e6f7d35676950dd7ae.

--- Saab95.java
+++ Saab95.java
@@ -7,7 +7,7 @@ public class Saab95{
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
-    public String modelName; // The car model name BAJS
+    public String modelName; // The car model name
     
     public Saab95(){
         nrDoors = 2;

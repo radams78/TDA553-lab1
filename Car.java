eab6b186831a5f7fd3fee0503b4dfa9f29c26f31
Kimshii <117647269+Kimshii@users.noreply.github.com>
2022-11-10 15:04:45 +0100

    Version1

---
+++ Car.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+
+public abstract class Car {
+
+    public int nrDoors;
+    public double enginePower;
+    public Color color;
+    public String modelName;
+    public double currentSpeed;
+
+    public Car(int nrDoors, double enginePower, Color color, String modelName) {
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        this.currentSpeed = 0;
+    }
+
+    public int getNrDoors() {
+        return this.nrDoors;
+    }
+
+    public double getEnginePower() {
+        return this.enginePower;
+    }
+
+    public double getCurrentSpeed() {
+        return this.currentSpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color clr) {
+        color = clr;
+    }
+
+    public void startEngine() {
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine() {
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    public abstract void incrementSpeed(double amount);
+
+    public abstract void decrementSpeed(double amount);
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount) {
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount) {
+        decrementSpeed(amount);
+    }
+}
---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args){
+
+        Car volvo = new Volvo240();
+        Car saab = new Saab95();
+
+        System.out.println(volvo.getEnginePower());
+        System.out.println(saab.modelName);
+
+    
+    }
+}

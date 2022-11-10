92b501901c41a68148944406c82b5eaf62d1472e
Duchessmuhr <alle.muhr@hotmail.com>
2022-11-10 14:17:02 +0100

    ja

--- Saab95.java
+++ Saab95.java
@@ -16,7 +16,7 @@ public class Saab95{
 	    turboOn = false;
         modelName = "Saab95";
         stopEngine();
-        System.out.println("hej");
+        System.out.println("hej123");
     }
     
     public int getNrDoors(){

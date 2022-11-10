67c91d506bc0652797a8da6196141c96b12a57fa
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-11-10 14:29:47 +0100

    Fixed test file

--- Test.java
+++ Test.java
@@ -1,8 +1,9 @@
 import java.lang.System;
+import java.awt.*;
 public class Test{
     public static void main(String[] args){
-        Saab95 saab = new Saab95();
-        Volvo240 volvo = new Volvo240();
+        Saab95 saab = new Saab95(Color.red);
+        Volvo240 volvo = new Volvo240(Color.black);
 
         System.out.println("Saab" + saab.getNrDoors());
         System.out.println("Volvo" + volvo.getNrDoors());

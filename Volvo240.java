523e045118a7a328033c2e3a8dd32ef85071da98
Martin Lidgren <martin.lidgren@live.se>
2022-11-10 14:03:05 +0100

    Implemented Car class

--- Volvo240.java
+++ Volvo240.java
@@ -1,15 +1,12 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Car{
 
     public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+
     
-    public Volvo240(){
+    public Volvo240 (){
+        super()
         nrDoors = 4;
         color = Color.black;
         enginePower = 100;
@@ -17,32 +14,6 @@ public class Volvo240{
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
-
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
     
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
@@ -56,13 +27,7 @@ public class Volvo240{
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
 
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
 
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
-    }
 }
+
+https://prod.liveshare.vsengsaas.visualstudio.com/join?5DC6DD470EFBFCB72AE0DEB741C7DFBB6683

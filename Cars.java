50346a2404fa62232e35c6654e5d772db4a03fe2
johannaavlund <johanna.avlund@hotmail.com>
2022-11-10 14:07:58 +0100

    lagt till superklass

---
+++ Cars.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+
+public class Cars {
+    private final int nrDoors;
+    private String modelName;
+    private double currentSpeed;
+    private double enginePower;
+    private Color color;
+
+
+    public Cars(String modelName, int nrDoors, double currentSpeed, double enginePower, Color color){
+        this.modelName = modelName;
+        this.nrDoors = nrDoors;
+        this.currentSpeed = currentSpeed;
+        this.enginePower = enginePower;
+        this.color = color;
+    }
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+}
--- Volvo240.java
+++ Volvo240.java
@@ -1,49 +1,22 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Cars{
 
     public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
+    public static int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    public static double currentSpeed; // The current speed of the car
+    public static Color color; // Color of the car
+    public static String modelName; // The car model name
     
     public Volvo240(){
+        super(modelName, nrDoors, currentSpeed, currentSpeed, color);
         nrDoors = 4;
         color = Color.black;
         enginePower = 100;
         modelName = "Volvo240";
         stopEngine();
     }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
-
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-    
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }

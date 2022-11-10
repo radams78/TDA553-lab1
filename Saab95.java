454dd0ee56a68540cd989815a60d81fbb14817a7
Martin Lidgren <martin.lidgren@live.se>
2022-11-10 13:50:56 +0100

    Update Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 
-public class Saab95{
+// TEST CHANGE
+
+public class Saab95 {
+
 
     public boolean turboOn;
     public int nrDoors; // Number of doors on the car
@@ -8,72 +11,77 @@ public class Saab95{
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
-    
-    public Saab95(){
+
+    public Saab95() {
         nrDoors = 2;
         color = Color.red;
         enginePower = 125;
-	    turboOn = false;
+        turboOn = false;
         modelName = "Saab95";
         stopEngine();
     }
-    
-    public int getNrDoors(){
+
+    public int getNrDoors() {
         return nrDoors;
     }
-    public double getEnginePower(){
+
+    public double getEnginePower() {
         return enginePower;
     }
 
-    public double getCurrentSpeed(){
+    public double getCurrentSpeed() {
         return currentSpeed;
     }
 
-    public Color getColor(){
+    public Color getColor() {
         return color;
     }
 
-    public void setColor(Color clr){
-	    color = clr;
+    public void setColor(Color clr) {
+        color = clr;
     }
 
-    public void startEngine(){
-	    currentSpeed = 0.1;
+    public void startEngine() {
+        currentSpeed = 0.1;
     }
 
-    public void stopEngine(){
-	    currentSpeed = 0;
+    public void stopEngine() {
+        currentSpeed = 0;
     }
 
-    public void setTurboOn(){
-	    turboOn = true;
+    public void setTurboOn() {
+        turboOn = true;
     }
 
-    public void setTurboOff(){
-	    turboOn = false;
+    public void setTurboOff() {
+        turboOn = false;
     }
-    
-    public double speedFactor(){
+
+    public double speedFactor() {
         double turbo = 1;
-        if(turboOn) turbo = 1.3;
+        if (turboOn)
+            turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
 
-    public void incrementSpeed(double amount){
+    public void incrementSpeed(double amount) {
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
     }
 
-    public void decrementSpeed(double amount){
+    public void decrementSpeed(double amount) {
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
-    
+
     // TODO fix this method according to lab pm
-    public void gas(double amount){
+    public void gas(double amount) {
         incrementSpeed(amount);
     }
 
     // TODO fix this method according to lab pm
-    public void brake(double amount){
+    public void brake(double amount) {
         decrementSpeed(amount);
     }
+
 }
+
+

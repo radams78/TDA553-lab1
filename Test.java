36dfb607aa908cfc11e277765e5f0d45e181af97
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-11-10 13:57:00 +0100

    created testing suite

--- Test.java
+++ Test.java
@@ -1,1 +1,10 @@
-//HELLO WORLD
+import java.lang.System;
+public class Test{
+    public static void main(String[] args){
+        Saab95 saab = new Saab95();
+        Volvo240 volvo = new Volvo240();
+
+        System.out.println("Saab" + saab.getNrDoors());
+        System.out.println("Volvo" + volvo.getNrDoors());
+    }
+}

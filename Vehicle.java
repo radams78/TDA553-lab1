8bf0cd95d1a0db0790c76e89effad5490557e6d8
Palpat3 <mandus.hogberg@gmail.com>
2022-11-10 14:42:35 +0100

    vehicle class filled

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,42 @@
-public class Vehicle {
+import java.awt.*;
+
+public abstract class Vehicle {
     
+    public double currentSpeed;
+    public Color color;
+    public String vehicleType;
+
+
+    public Vehicle(String vehicleType, Color color, double currentSpeed){
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.currentSpeed = currentSpeed;
+
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    
+    public abstract double speedFactor();
+    
+
+
+    public abstract void incrementSpeed(double amount);
+        
+    
+
+    public abstract void decrementSpeed(double amount);
+        
+    
+
 }

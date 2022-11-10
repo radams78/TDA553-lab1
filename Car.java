18919d1a25f868f93f351eb844f3943881e87bb9
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-11-10 13:32:51 +0100

    Created initializer

--- Car.java
+++ Car.java
@@ -1,3 +1,8 @@
-public class Car {
-    
+public abstract class Car {
+    public Car(int nrDoors, Color color, int enginePower, String modelName){
+        this.nrDoors = nrDoors;
+        this.color = color;
+        this.enginePower = enginePower;
+        this.modelName = modelName;
+    }
 }

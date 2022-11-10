2ee9a95dbb24de53d5fd2892b4172d608a5303c0
Sebbe213 <sebastianraoof@hotmail.se>
2022-11-10 15:44:27 +0100

    changes in class cars

--- cars.java
+++ cars.java
@@ -1,13 +1,52 @@
-public class cars {
+public class Cars {
 
     public int nrDoors;
     public Color color;  
     public double enginePower; 
-	public boolean turboOn;
     public String modelName; 
-    stopEngine();
 
+public Cars(int nrDoors, Color color, double enginePower, string modelName){
+    this.nrDoors = nrDoors;
+    this.color = color;
+    this.enginePower = enginePower
+    this.modelName = modelName
+
+}
     
+public int getNrDoors(){
+    return nrDoors;
+}
+public double getEnginePower(){
+    return enginePower;
+}
+
+public double getCurrentSpeed(){
+    return currentSpeed;
+}
+
+public Color getColor(){
+    return color;
+}
+
+public void setColor(Color clr){
+    color = clr;
+}
+
+public void startEngine(){
+   
+}
+
+public void stopEngine(){
+    
+}
+
+public abstract double speedFactor();
+
+public abstract void incrementSpeed();
+    
+
+
+
 
 
 }

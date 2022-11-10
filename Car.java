0231e0443a9e9ab8ada7051837dfa3124e2af1ed
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2022-11-10 14:39:08 +0100

    Add car base class and move common properties

---
+++ Car.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+
+public abstract class Car {
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power
+    private double currentSpeed; // Current speed
+    private Color color; // Color of the car
+    private String modelName; // Name of the model
+
+    protected Car(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName) {
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.currentSpeed = currentSpeed;
+        this.color = color;
+        this.modelName = modelName;
+    }
+
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+
+    
+
+    public abstract void incrementSpeed(double amount);
+
+    public abstract void decrementSpeed(double amount);
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+
+    public void setNrDoors(int nrDoors) {
+        this.nrDoors = nrDoors;
+    }
+
+    public double getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(double enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(double currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+}
--- Saab95.java
+++ Saab95.java
@@ -1,50 +1,14 @@
 import java.awt.*;
 
-public class Saab95{
-
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+public class Saab95  extends Car{
+    private boolean turboOn;
     
-    public Saab95(){
-        nrDoors = 2;
-        color = Color.red;
-        enginePower = 125;
-	    turboOn = false;
-        modelName = "Saab95";
+    public Saab95(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName, boolean turboOn) {
+        super(nrDoors, enginePower, currentSpeed, color, modelName);
+        this.turboOn = turboOn;
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
-
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-
     public void setTurboOn(){
 	    turboOn = true;
     }
@@ -66,7 +30,7 @@ public class Saab95{
     public void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
-    
+   
     // TODO fix this method according to lab pm
     public void gas(double amount){
         incrementSpeed(amount);

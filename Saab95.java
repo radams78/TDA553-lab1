589a136bdfb17f4f818ff1e6f7d35676950dd7ae
Programmer64fortnite <84543653+Programmer64fortnite@users.noreply.github.com>
2022-11-10 14:39:16 +0100

    Update Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -7,7 +7,7 @@ public class Saab95{
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
-    public String modelName; // The car model name
+    public String modelName; // The car model name BAJS
     
     public Saab95(){
         nrDoors = 2;

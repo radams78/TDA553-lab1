a3fde5d80ce6a24c4b0e7053e35c930ab941f08a
Sebbe213 <sebastianraoof@hotmail.se>
2022-11-10 14:25:04 +0100

    I have added attributes

--- cars.java
+++ cars.java
@@ -1,3 +1,13 @@
 public class cars {
+
+    public int nrDoors;
+    public Color color;  
+    public double enginePower; 
+	public boolean turboOn;
+    public String modelName; 
+    stopEngine();
+
     
+
+
 }

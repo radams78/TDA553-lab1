952dc1bdcb90bce82a086f33c00c3acbb8eb05d5
OxyRust <97917116+OxyRust@users.noreply.github.com>
2022-11-11 10:26:13 +0100

    Added car extends Movable

--- Car.java
+++ Car.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public abstract class Car {
+public abstract class Car extends Movable {
     private int nrDoors;
     private Color color;
     protected int enginePower;
@@ -42,6 +42,23 @@ public abstract class Car {
     public void stopEngine(){
         currentSpeed = 0;
     }
+
+    public void move(){
+
+    }
+
+    public void turnLeft(){
+
+    }
+
+    public void turnRight(){
+
+    }
+
+    public void stop(){
+        
+    }
+
 
     // TODO fix this method according to lab pm
     public void gas(double amount){
--- Saab95.java
+++ Saab95.java
@@ -23,6 +23,5 @@ public class Saab95 extends Car{
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
-
     
 }

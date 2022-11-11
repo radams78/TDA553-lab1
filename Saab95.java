9bccc2eeb94ef56e553f672c30679f63bb2db8fe
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2022-11-11 14:02:17 +0100

    Tagit bort dubbletter.

--- Saab95.java
+++ Saab95.java
@@ -2,12 +2,7 @@ import java.awt.*;
 
 public class Saab95 extends Car{
 
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    private boolean turboOn;
     
     public Saab95(){
         super();
@@ -18,34 +13,6 @@ public class Saab95 extends Car{
         modelName = "Saab95";
         stopEngine();
     }
-    
-    // Permission to remove this?
-    // public int getNrDoors(){ 
-    //     return nrDoors;
-    // }
-    // public double getEnginePower(){
-    //     return enginePower;
-    // }
-
-    // public double getCurrentSpeed(){
-    //     return currentSpeed;
-    // }
-
-    // public Color getColor(){
-    //     return color;
-    // }
-
-    // public void setColor(Color clr){
-	//     color = clr;
-    // }
-
-    // public void startEngine(){
-	//     currentSpeed = 0.1;
-    // }
-
-    // public void stopEngine(){
-	//     currentSpeed = 0;
-    // }
 
     public void setTurboOn(){
 	    turboOn = true;
--- Volvo240.java
+++ Volvo240.java
@@ -2,13 +2,7 @@ import java.awt.*;
 
 public class Volvo240 extends Car{
 
-    public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    public String davidtest;
+    private final static double trimFactor = 1.25;
 
     public Volvo240(){
         super();
@@ -18,35 +12,7 @@ public class Volvo240 extends Car{
         modelName = "Volvo240";
         stopEngine();
     }
-    
-    // Permission to remove this?
-    // public int getNrDoors(){
-    //     return nrDoors;
-    // }
-    // public double getEnginePower(){
-    //     return enginePower;
-    // }
-
-    // public double getCurrentSpeed(){
-    //     return currentSpeed;
-    // }
-
-    // public Color getColor(){
-    //     return color;
-    // }
-
-    // public void setColor(Color clr){
-	//     color = clr;
-    // }
-
-    // public void startEngine(){
-	//     currentSpeed = 0.1;
-    // }
-
-    // public void stopEngine(){
-	//     currentSpeed = 0;
-    // }
-    
+        
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }

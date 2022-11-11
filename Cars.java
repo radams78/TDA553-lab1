d7099a1f7d2c282216493eabe99b9a1b1827d8b9
Gabriel2000001 <radwanr@net.chalmers.se>
2022-11-11 15:01:19 +0100

    I introduced five new instances that represent the base of a simplified matrx rotation based on a "1d object".

--- Cars.java
+++ Cars.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+
 public abstract class Cars implements Movable {
    
     protected int nrDoors;
@@ -6,65 +7,84 @@ public abstract class Cars implements Movable {
     protected double enginePower; 
     protected String modelName; 
     protected double currentSpeed;
+    protected double xCoord_1;
+    protected double xCoord_2;
+    protected double yCoord_1;
+    protected double yCoord_2;
+    protected double center;
 
-    private int xCoord;
-
-    protected Cars(int nrDoors, Color color, double enginePower, String modelName, double currentSpeed){
+    protected Cars(int nrDoors, Color color, double enginePower, String modelName, double currentSpeed, double xCoord_1, double xCoord_2, double yCoord_1, double yCoord_2, double center){
     this.nrDoors = nrDoors;
     this.color = color;
     this.enginePower = enginePower;
     this.modelName = modelName;
     this.currentSpeed = currentSpeed;
-    this.xCoord = 0;
-    
+    this.xCoord_1 = xCoord_1;
+    this.xCoord_2 = xCoord_2;
+    this.yCoord_1 = yCoord_1;
+    this.yCoord_2 = yCoord_2;
+    this.center = center;
+
 }
 
+    public void turnRight(){
+    }
 
-public void turnRight(){
+    public void turnLeft(){
+    }
 
-}
+    public void move(){
+    }
 
-public void turnLeft(){
-}
+    protected double getxCoord_1() {
+        return xCoord_1;
+    }
+    protected double getxCoord_2() {
+        return xCoord_2;
+    }
+    protected double getyCoord_1() {
+        return yCoord_1;
+    }
+    protected double getyCoord_2() {
+        return yCoord_2;
+    }
+    protected double getCenter() {
+        return center;
+    }
 
-public void move(){
-    xCoord += currentSpeed;
-}
 
+    protected int getNrDoors(){
+        return nrDoors;
+    }
+    protected double getEnginePower(){
+        return enginePower;
+    }
 
-protected int getNrDoors(){
-    return nrDoors;
-}
-protected double getEnginePower(){
-    return enginePower;
-}
+    protected double getCurrentSpeed(){
+        return currentSpeed;
+    }
 
-protected double getCurrentSpeed(){
-    return currentSpeed;
-}
+    protected Color getColor(){
+        return color;
+    }
 
-protected Color getColor(){
-    return color;
-}
+    protected void setColor(Color clr){
+        color = clr;
+    }
 
-protected void setColor(Color clr){
-    color = clr;
-}
+    protected void startEngine(){
+       currentSpeed = 0.1;
+    }
 
-protected void startEngine(){
-   currentSpeed = 0.1;
-}
+    protected void stopEngine(){
+        currentSpeed = 0;
+    }
 
-protected void stopEngine(){
-    currentSpeed = 0;
-}
+    protected abstract double speedFactor(double amount);
 
-protected abstract double speedFactor(double amount);
+    protected abstract void incrementSpeed(double amount);
 
-protected abstract void incrementSpeed(double amount);
+    protected abstract void decrementSpeed(double amount);
 
-protected abstract void decrementSpeed(double amount);
-
-    
 }
 

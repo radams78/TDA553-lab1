da3bc00a229f87c018cb0a95a8c1bec0ded637c9
McQueenMG <117281156+McQueenMG@users.noreply.github.com>
2022-11-11 13:09:11 +0100

    done

--- Run.java
+++ Run.java
@@ -2,5 +2,7 @@ public class Run {
     public static void main(String[] args) {
         Volvo240 volvo = new Volvo240();
         System.out.println("\n" + volvo.toString());
+        Saab95 saab = new Saab95();
+        System.out.println("\n" + saab.toString());
     }
 }

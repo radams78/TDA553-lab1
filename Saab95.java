8c6261c7875ee99c97ea1eb348cb4a1ec67b2da8
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-11-11 13:35:04 +0100

    Finished heritance

--- Saab95.java
+++ Saab95.java
@@ -1,24 +1,16 @@
 import java.awt.*;
 
-public class Saab95 extends Car{
+import javax.swing.text.StyledEditorKit.BoldAction;
 
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
-    public Saab95(){
-        nrDoors = 2;
-        color = Color.red;
-        enginePower = 125;
-	    turboOn = false;
-        modelName = "Saab95";
+public class Saab95 extends Car{
+    private Boolean turboOn;
+
+    public Saab95(int nrDoors, double enginePower, double currentSpeed, Color color){
+        super(nrDoors, enginePower, currentSpeed, color);
+        turboOn = false;
         stopEngine();
     }
-    
-//jaja
+
     public void setTurboOn(){
 	    turboOn = true;
     }
@@ -30,15 +22,17 @@ public class Saab95 extends Car{
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
-        return enginePower * 0.01 * turbo;
+        return getEnginePower() * 0.01 * turbo;
     }
 
     public void incrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+        double currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+        setCurrentSpeed(currentSpeed);
     }
 
     public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+        double currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+        setCurrentSpeed(currentSpeed);
     }
     
 }

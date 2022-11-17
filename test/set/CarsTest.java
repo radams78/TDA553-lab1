fbacb98b84224582ac3159ebd53d85ee322e7457
McQueenMG <117281156+McQueenMG@users.noreply.github.com>
2022-11-17 13:58:15 +0100

    Co-authored-by: hannistuis <hannistuis@users.noreply.github.com> Co-authored-by: WinkiWonderland <WinkiWonderland@users.noreply.github.com>

--- test/set/CarsTest.java
+++ test/set/CarsTest.java
@@ -1,12 +1,35 @@
 package set;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+
+
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
 
 
 public class CarsTest {
     @Test
-    public void testing(){
-        assertEquals(0, 0);
+    public void testing_if_instance_of_superclass(){
+
+        Volvo240 volvo = new Volvo240();
+        Saab95 saab = new Saab95();
+        assertTrue(volvo instanceof Cars);
+        assertTrue(saab instanceof Cars);
     }
+    public void check_speed_of_cars(){
+
+        Volvo240 volvo = new Volvo240(); //Doesn't need to test both bc they're under cars, same method
+        volvo.incrementSpeed(10);
+        assertEquals(12.5, volvo.getX());
+
+    }
+
+    public void check_move_method(){
+
+        Volvo240 volvo = new Volvo240();
+        volvo.incrementSpeed(10);
+        volvo.move();
+    }
+
 }

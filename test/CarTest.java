fdc2ea334717de5e3aa0d3fe1321284cd9f0cc71
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-11-17 13:38:41 +0100

    Test map

---
+++ test/CarTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class CarTest {
+    @Test
+    public void empty_set_should_have_size_zero() {
+        assertEquals(0, );
+      }
+}

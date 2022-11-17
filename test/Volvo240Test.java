80e8410312bc330c3fea4b03c35a7497e42d8876
David-Pixeln <kalmarkiwi03@gmail.com>
2022-11-17 15:57:21 +0100

    Fixed Semicolons + TestsOutOfRange in Volvo

--- test/Volvo240Test.java
+++ test/Volvo240Test.java
@@ -2,6 +2,7 @@ package test;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 import src.*;
@@ -22,16 +23,34 @@ public class Volvo240Test {
     }
 
     @Test
-    public void testGas(){
+    public void testGasInRange(){
         Car test = new Volvo240();
-        test.gas(10);
-        assertEquals(12.5, test.getCurrentSpeed(), 0.1);
+        test.gas(0.5);
+        assertEquals(0.625, test.getCurrentSpeed(), 0.1);
     }
 
     @Test
-    public void testBrake(){
+    public void testBrakeInRange(){
+        Car test = new Volvo240();
+        test.brake(1);
+        assertEquals(0, test.getCurrentSpeed(), 0.1);
+    }
+
+    @Test
+    public void testGasOutOfRange(){
+        Car test = new Volvo240();
+        try{
+            test.brake(10);
+        } catch {
+            
+        }
+        assertThrows(IllegalArgumentException.class, ()->{});
+    }
+
+    @Test
+    public void testBrakeOutOfRange(){
         Car test = new Volvo240();
         test.brake(10);
-        assertEquals(0, test.getCurrentSpeed(), 0.1);
+        assertThrows(IllegalArgumentException.class, ()->{});
     }
 }

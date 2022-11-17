3aa967505cbc8dd39b6ee2a6c1956d23463532c0
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2022-11-17 15:00:04 +0100

    Fix gameloop

--- Driver.java
+++ Driver.java
@@ -1,15 +1,14 @@
 public class Driver {
     Car car1;
     Car car2;
-
-    
-
 public static void main(String[] args) { // Required to run the program
     Car car1 = new Volvo240(); 
     Car car2 = new Saab95();
     while (true){
         new Movable().move(car1);
         new Movable().move(car2);
+        System.out.printf("The location of the Volvo is x: %d, Y: %d", car1.getX(), car1.getY());
+        System.out.printf("The location of the Volvo is x: %d, Y: %d", car2.getX(), car2.getY());
     }
 
 }

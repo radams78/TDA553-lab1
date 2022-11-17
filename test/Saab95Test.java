9dccac9c19da0bf47db9541198e1c399d28f08e8
aronlundh <Lundh.aron@gmail.com>
2022-11-17 15:53:20 +0100

    Mer tester

--- test/Saab95Test.java
+++ test/Saab95Test.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.awt.*;
 
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,15 @@ public class Saab95Test {
     @Test
     public void speedFactor_Should_Be_20(){
         Saab95 saab95 = new Saab95(4, 2000.0, 100.0, Color.RED);
-        assertEquals(20, saab95.speedFactor(), 0.05);
+        assertEquals(20, saab95.speedFactor());
     }
+
+    @Test
+    public void incrementSpeed_Should_Be_20(){
+        Saab95 saab95 = new Saab95(4, 2000.0, 100.0, Color.RED);
+        double oldCurrentSpeed = saab95.getCurrentSpeed();
+        saab95.incrementSpeed(2);
+        double diffOlSpdNewSpd = saab95.getCurrentSpeed() - oldCurrentSpeed;
+        assertEquals(140, diffOlSpdNewSpd);
+    } 
 }

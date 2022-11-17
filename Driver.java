0d8463b21694bd4527a5429830d6a7eda76156cc
Pdoxxy <117639216+Pdoxxy@users.noreply.github.com>
2022-11-17 14:54:17 +0100

    .

--- Driver.java
+++ Driver.java
@@ -1,41 +1,34 @@
 public class Driver {
+    Car car1;
+    Car car2;
 
     
-    Saab95 car2 = new Saab95();
 
 public static void main(String[] args) { // Required to run the program
-    new Driver().goCar1(); 
-    new Driver().goCar2(); 
-}
-
-public void goCar1() {
-    Car car1 = new Volvo240();
-    move(car1);
-    System.out.println(car1.color);
-    System.out.println(car1.modelName);
-    System.out.println(car1.currentSpeed);
-    System.out.println(car1.speedFactor());
-}
-
-public void goCar2() {
+    Car car1 = new Volvo240(); 
     Car car2 = new Saab95();
-    move(car2);
-    System.out.println(car2.color);
-    System.out.println(car2.modelName);
-    System.out.println(car2.currentSpeed);
-    System.out.println(car2.speedFactor());
-}
-
-public void move(Car theCar){
-    new Movable().incrementSpeed(1, theCar);
-}
-
-public void turnLeft(){
+    while (true){
+        new Movable().move(car1);
+        new Movable().move(car2);
+    }
 
 }
 
-public void turnRight(){
+// public void goCar1() {
+//     Car car1 = new Volvo240();
+//     new Movable().move(car1);
+//     System.out.println(car1.color);
+//     System.out.println(car1.modelName);
+//     System.out.println(car1.currentSpeed);
+//     System.out.println(car1.speedFactor());
+// }
 
-}
-
+// public void goCar2() {
+//     Car car2 = new Saab95();
+//     new Movable().move(car2);
+//     System.out.println(car2.color);
+//     System.out.println(car2.modelName);
+//     System.out.println(car2.currentSpeed);
+//     System.out.println(car2.speedFactor());
+// }
 }

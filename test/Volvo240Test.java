60c95676c23cacb907f4bf7c7a40c1603ffeccd7
HugoUnosson <hugo.unosson@gmail.com>
2022-11-18 11:34:06 +0100

    Tester

--- test/Volvo240Test.java
+++ test/Volvo240Test.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import java.awt.*;
 
@@ -10,6 +12,22 @@ public class Volvo240Test {
         assertEquals(12.5, volvo240.speedFactor(), 0.05);
     }
 
-    
+    @Test
+    public void incrementSpeed_Should_Be_25(){
+        Volvo240 volvo240 = new Volvo240(4, 1000.0, 100.0, Color.BLUE);
+        double oldCurrentSpeed = volvo240.getCurrentSpeed();
+        volvo240.incrementSpeed(2);
+        double diffOlSpdNewSpd = volvo240.getCurrentSpeed() - oldCurrentSpeed;
+        assertEquals(25, diffOlSpdNewSpd);
+    } 
+
+    @Test
+    public void decrementSpeed_Should_Be_20(){
+        Volvo240 volvo240 = new Volvo240(4, 1000.0, 100.0, Color.BLUE);
+        double oldCurrentSpeed = volvo240.getCurrentSpeed();
+        volvo240.decrementSpeed(1);
+        double diffOlSpdNewSpd = volvo240.getCurrentSpeed() - oldCurrentSpeed;
+        assertEquals(0, diffOlSpdNewSpd);
+    } 
 }
 

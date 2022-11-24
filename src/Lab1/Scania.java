9f36ddca5cf0ef78dfecfcb0c46292f03766aabb
Simon Westlin Green <simon.green@live.se>
2022-11-24 13:06:44 +0100

    Changed angles to private

--- src/Lab1/Scania.java
+++ src/Lab1/Scania.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Scania extends Car {
     private double platformAngle = 0d;
-    public double maxAngle = 70d;
-    public double lowestAngle = 0d;
+    private double maxAngle = 70d;
+    private double lowestAngle = 0d;
 
     public Scania(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName,
             double platformAngle) {
@@ -19,31 +19,30 @@ public class Scania extends Car {
         return platformAngle;
     }
 
-    public void setPlatformAngle(double currentAngle){
-        if(getCurrentSpeed() == 0){
-            if(currentAngle > maxAngle){
+    public void setPlatformAngle(double currentAngle) {
+        if (getCurrentSpeed() == 0) {
+            if (currentAngle > maxAngle) {
                 this.platformAngle = maxAngle;
-    
-            } else if(currentAngle < lowestAngle){
+
+            } else if (currentAngle < lowestAngle) {
                 this.platformAngle = lowestAngle;
-    
-            } else{
+
+            } else {
                 this.platformAngle = currentAngle;
             }
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Cant set platform at that angle if car is moving");
         }
-        
-}
 
-    public void raisePlatform(double amount){
+    }
+
+    public void raisePlatform(double amount) {
         setPlatformAngle(getPlatformAngle() + amount);
     }
 
     public void lowerPlatform(double amount) {
         setPlatformAngle(getPlatformAngle() - amount);
-        
+
     }
 
     @Override

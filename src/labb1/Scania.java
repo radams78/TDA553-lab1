a7da13f2920f4eabf8da54914b250fadbdf8b5bc
OxyRust <97917116+OxyRust@users.noreply.github.com>
2022-11-24 14:20:00 +0100

    Created Scania and maid platform movable

--- src/labb1/Scania.java
+++ src/labb1/Scania.java
@@ -1,7 +1,38 @@
-// package labb1;
+package labb1;
 
-// import java.awt.*;
+import java.awt.*;
 
-// public class Scania extends Car{
+public class Scania extends Truck{
+    private final int MAX_ANGLE = 70; //Maximum angle extention
+    private final int MIN_ANGLE = 0; //Minimum angle extention
 
-// }
+    private int platformAngle;
+
+    public Scania(Color color, int enginePower, String modelName, double x, double y, double dirX, double dirY, int maxLoad){
+        super(color, enginePower, modelName, x, y, dirX,  dirY, maxLoad);
+        this.platformAngle = 0;
+    }
+
+    @Override 
+    public double speedFactor(){
+        double speedFactor = 1.001;
+        return speedFactor;
+    }
+
+    public void raisePlatform(int angle){
+        if (this.platformAngle <= MAX_ANGLE){
+        this.platformAngle += angle;
+        }else{
+            this.platformAngle = MAX_ANGLE;
+        }
+    }
+
+    public void lowerPlatform(int angle){
+        if (this.platformAngle >= MIN_ANGLE){
+            this.platformAngle -= angle;
+            }else{
+                this.platformAngle = MIN_ANGLE;
+            }
+    }
+
+}

d2964c5ae64f0b77d15b2013841500193a9563a4
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-11-24 14:56:27 +0100

    made truc only gas if bed is not extended

--- src/labb1/Truck.java
+++ src/labb1/Truck.java
@@ -5,14 +5,38 @@ import java.awt.*;
 //*Abstract class for any truck. Truck must have a max load . */
 public abstract class Truck extends Vehicle{
     private int maxLoad;
+    private Boolean platformExtended;
     public Truck(Color color, int enginePower, String modelName, double  x, double y, double dirX, double dirY, int maxLoad){
         super(2, color, enginePower, modelName,   x,  y, dirX,  dirY);
         this.maxLoad = maxLoad;
+        this.platformExtended = false;
     }
 
     // Returns max load var
     public int getMaxLoad(){
         return maxLoad;
+    }
+
+    public Boolean isPlatformExtended(){
+        return platformExtended;
+    }
+
+    public void extendPlatform(){
+        platformExtended = true;
+    }
+
+    public void retractPlatform(){
+        platformExtended = false;
+    }
+    
+    @Override
+    public void gas(double amount){
+        if (platformExtended){
+            throw new IllegalArgumentException("Hey stop");
+        }else{
+            super.gas(amount);
+        }
+        
     }
 
 

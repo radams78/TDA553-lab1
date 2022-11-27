68e7151854b723440437c520969bc0bb82a5f063
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-11-27 14:27:44 +0100

    fixat bugen tror jag

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,7 @@
 
 import java.awt.*;
 
-public abstract class Car extends Vehicle{
+public abstract class Car extends Vehicle {
     public Car(int nrDoors, double enginePower, double currentSpeed, Color color) {
         super(nrDoors, enginePower, currentSpeed, color);
 

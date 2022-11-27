afa0e687b1349734712b3c551e91df6550f42592
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-11-27 15:13:15 +0100

    Constructor for Scania and speedfactor()

--- src/Scania.java
+++ src/Scania.java
@@ -1,3 +1,9 @@
+import java.awt.*;
+
 public class Scania extends Truck{
-    
+    public Scania(int nrDoors, double enginePower, double currentSpeed, Color color){
+        super(nrDoors, enginePower, currentSpeed, color);
+        stopEngine();
+    }
+
 }
--- src/Truck.java
+++ src/Truck.java
@@ -1,10 +1,14 @@
 import java.awt.*;
 
-public abstract class Truck extends Vehicle{
+public class Truck extends Vehicle{
     public Truck(int nrDoors, double enginePower, double currentSpeed, Color color) {
         super(nrDoors, enginePower, currentSpeed, color);
 
         // this.modelName = modelName;
+    }
+
+    public double speedFactor(){
+        return getEnginePower() * 0.01;
     }
 
 }

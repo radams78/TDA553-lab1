6e96ed38492775bc6348eac600693df48ea3461a
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2022-11-28 10:40:58 +0100

    Add flatbed class

---
+++ src/Lab1/Flatbed.java
@@ -0,0 +1,46 @@
+package Lab1;
+
+public class Flatbed {
+    private double currAngle = 0d;
+    private double maxAngle = 70d;
+    private double minAngle = 0d;
+
+   public Flatbed(double maxAngle, double minAngle) {
+        this.maxAngle = maxAngle;
+        this.minAngle = minAngle;
+   } 
+
+   public double getcurrAngle() {
+    return currAngle;
+}
+
+public void setCurrAngle(double currentAngle) {
+        if (currentAngle > maxAngle) {
+            this.currAngle = maxAngle;
+
+        } else if (currentAngle < minAngle) {
+            this.currAngle = minAngle;
+
+        } else {
+            this.currAngle = currentAngle;
+        }
+        // TODO Add check if vehicle is moving in truck
+
+}
+
+public void raisePlatform(double amount) {
+    setCurrAngle(getcurrAngle() + amount);
+}
+
+public void lowerPlatform(double amount) {
+    setCurrAngle(getcurrAngle() - amount);
+}
+
+public double getMaxAngle() {
+    return maxAngle;
+}
+
+public double getminAngle() {
+    return minAngle;
+}
+}

287e874882588ba024ba1cdf1869645ec2213c15
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2022-11-28 14:15:03 +0100

    Add CarRepairShop class

---
+++ src/Lab1/CarRepairShop.java
@@ -0,0 +1,23 @@
+package Lab1;
+
+import java.util.Set;
+
+public class CarRepairShop {
+    Set<Car> carsInShop;
+    int x;
+    int y;
+    int capacity;
+    public CarRepairShop(int x, int y, int capacity) {
+        this.x = x;
+        this.y = y;
+        this.capacity = capacity;
+    }
+
+    public void loadCar(Car car) {
+        carsInShop.add(car);
+    }
+
+    public void unloadCar(Car car) {
+        carsInShop.remove(car);
+    }
+}

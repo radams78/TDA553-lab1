aa0819fe6c667de2aa18731b3d0ab046cb2b3fde
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-11-29 11:12:41 +0100

    Made speedfactor protected. Fixed issue #4

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -9,7 +9,7 @@ public class Volvo240 extends Car{
         stopEngine();
     }
     
-    public double speedFactor(){
+    protected double speedFactor(){
         return getEnginePower() * 0.01 * trimFactor;
     }
 

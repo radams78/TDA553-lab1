4f8f88be0b3d7d1f25ffc5fadaec0bd79f9664e0
Aron Lundh <lundh.aron@gmail.com>
2022-11-30 14:39:27 +0100

    Gjorde konstruktor, och getters and setters i CRS

--- src/CarRepairShop.java
+++ src/CarRepairShop.java
@@ -1,5 +1,27 @@
-public class CarRepairShop {
-    public CarRepairShop(int MaxNrOfCars){
+public class CarRepairShop implements Load{
+    private int maxNrOfCars;
+    private int currentNrOfCars;
+    
+    public CarRepairShop(int maxNrOfCars, int currentNrOfCars){
+        this.maxNrOfCars = Math.max(maxNrOfCars, 0);
+        this.currentNrOfCars = Math.max(currentNrOfCars, 0);
+    }
+    
+    public int getCurrentNrOfCars() {
+        return currentNrOfCars;
+    }
+
+    public void setCurrentNrOfCars(int currentNrOfCars) {
+        this.currentNrOfCars = currentNrOfCars;
+    }
+
+    public int getMaxNrOfCars(){
+        return maxNrOfCars;
+    }
+
+    @Override
+    public void load() {
+        
         
     }
 }

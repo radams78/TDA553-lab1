80e7e8c81e470645441c9bb3ccc1aeda3b42bb63
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-11-30 15:07:28 +0100

    Implementing the method that should be overriden

--- src/Scania.java
+++ src/Scania.java
@@ -1,13 +1,20 @@
 import java.awt.*;
 
 public class Scania extends Truck{
-    private Flatbed flatbed = new DegreeFlatbed();
+    private Flatbed flatbed;
 
     public Scania(int nrDoors, double enginePower, double currentSpeed, Color color){
         super(nrDoors, enginePower, currentSpeed, color);
+        this.flatbed = new DegreeFlatbed();
     }
 
     @Override
-    
+    public void startEngine() {
+        
+    }
 
+    @Override
+    public void gas(double gas){
+        
+    }
 }

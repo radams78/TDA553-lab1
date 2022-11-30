d3cf482208d311ab7d508ec143cffc9dd9dbd918
Simon Westlin Green <simon.green@live.se>
2022-11-30 13:32:49 +0100

    Fixed import

--- src/Lab1/CarRepairShop.java
+++ src/Lab1/CarRepairShop.java
@@ -1,13 +1,14 @@
 package Lab1;
 
 import java.util.Set;
-import  Lab1.vehicles.*;
+import Lab1.vehicles.Car;
 
 public class CarRepairShop {
     Set<Car> carsInShop;
     int x;
     int y;
     int capacity;
+
     public CarRepairShop(int x, int y, int capacity) {
         this.x = x;
         this.y = y;

e8c2743cc7ed62858ac94b65d1f8151cc73dc93b
Aron Lundh <lundh.aron@gmail.com>
2022-11-30 15:14:03 +0100

    Gjorde ändringar i CarRepairShop

---
+++ test/CarRepairShopTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CarRepairShopTest{
+    @Test
+    public void max_Number_Of_Cars_should_not_be_below_1(){
+        CarRepairShop carRepairShop = new CarRepairShop(5, 0);
+        
+    }
+}

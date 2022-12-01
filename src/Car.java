1bb306dde19bf165f74866f7530498a6adaae928
HugoUnosson <hugo.unosson@gmail.com>
2022-12-01 15:02:04 +0100

    Load condition

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,14 @@
 import java.awt.*;
 
 public abstract class Car extends Vehicle {
+    private boolean loaded;
+
     public Car(int nrDoors, double enginePower, double currentSpeed, Color color) {
         super(nrDoors, enginePower, currentSpeed, color);
+        this.loaded = false;
+    }
+
+    private void setLoaded(boolean trueOrFalse) {
+        this.loaded = trueOrFalse;
     }
 }

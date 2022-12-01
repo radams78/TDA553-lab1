883ded16187875173d218c5a0393465774bf33dd
Aron Lundh <lundh.aron@gmail.com>
2022-12-01 15:20:32 +0100

    CRST more tests

--- test/CarRepairShopTest.java
+++ test/CarRepairShopTest.java
@@ -24,6 +24,21 @@ public class CarRepairShopTest{
         CarRepairShop MECA = new CarRepairShop(5, 0, 1, 1);
         Car saab95 = new Saab95(4, 50, 50, Color.RED);
         MECA.load(saab95);
+        assertTrue(saab95.loaded);
     }
-
+    @Test
+    public void unload_is_able_to_unload(){
+        CarRepairShop mekonomen = new CarRepairShop(5, 0, 1, 1);
+        Car finBil = new Volvo240(4, 150, 20, Color.GREEN);
+        mekonomen.load(finBil);
+        mekonomen.unload(finBil);
+        assertFalse(finBil.loaded);
+    }
+    @Test
+    public void load_should_not_load(){
+        CarRepairShop jönssonsBilverkstad = new CarRepairShop(2, 0, 4, 4);
+        Car janssonsBil = new Volvo240(4, 10, 10, Color.YELLOW);
+        jönssonsBilverkstad.load(janssonsBil);
+        assertFalse(janssonsBil.loaded);
+    }
 }

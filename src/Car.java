08af9e8ea5adadc5f9ef11bfe6d452a51edf4585
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-12-01 15:56:36 +0100

    name change

--- src/Car.java
+++ src/Car.java
@@ -8,8 +8,8 @@ public abstract class Car extends Vehicle {
         this.loaded = false;
     }
 
-    public void setLoaded(boolean trueOrFalse) {
-        this.loaded = trueOrFalse;
+    public void setLoaded(boolean loaded) {
+        this.loaded = loaded;
     }
     
 }

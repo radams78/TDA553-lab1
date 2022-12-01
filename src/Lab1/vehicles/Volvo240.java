8f66c2d07826c183119daa51396d2cec32ddc4b4
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2022-12-01 13:30:42 +0100

    change visibility of trim factor

--- src/Lab1/vehicles/Volvo240.java
+++ src/Lab1/vehicles/Volvo240.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Volvo240 extends Car implements ITrimmable {
 
-    public final static double trimFactor = 1.25;
+    private final static double trimFactor = 1.25;
 
     public Volvo240(int nrDoors, double enginePower, double currentSpeed, Color color, String modelName) {
         super(nrDoors, enginePower, currentSpeed, color, modelName);
@@ -14,6 +14,10 @@ public class Volvo240 extends Car implements ITrimmable {
     @Override
     public double speedFactor() {
         return getEnginePower() * 0.01 * trimFactor;
+    }
+
+    public double getTrimFactor() {
+        return trimFactor;
     }
 
 }

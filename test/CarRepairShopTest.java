6edd3251d1a88462c034ddc4c6c170d5dde55c9a
David-Pixeln <kalmarkiwi03@gmail.com>
2022-12-02 10:27:47 +0100

    Added a few tests to CarRepairShop

--- test/CarRepairShopTest.java
+++ test/CarRepairShopTest.java
@@ -13,5 +13,29 @@ import org.junit.Test;
 import src.*;
 
 public class CarRepairShopTest {
-    @
+    @Test
+    public void canLoadCar() {
+        Volvo240 testCar = new Volvo240();
+        CarRepairShop testShop = new CarRepairShop(2, 10, 15);
+        testShop.loadCar(testCar);
+        assertEquals(true, testShop.isCarInLoad(testCar));
+    }
+
+    @Test
+    public void canUnloadCar() {
+        Volvo240 testCar = new Volvo240();
+        CarRepairShop testShop = new CarRepairShop(2, 10, 15);
+        testShop.loadCar(testCar);
+        testShop.unloadCar(testCar);
+        assertEquals(false, testShop.isCarInLoad(testCar));
+    }
+
+    @Test
+    public void cantLoadCarTooFarAway() {
+        Volvo240 testCar = new Volvo240();
+        CarRepairShop testShop = new CarRepairShop(2, 10, 15);
+        testShop.loadCar(testCar);
+        testShop.unloadCar(testCar);
+        assertEquals(false, testShop.isCarInLoad(testCar));
+    }
 }

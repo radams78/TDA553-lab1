b5503f9798acc085af044206974f92cf85182a3a
hannapanna3 <117651241+hannapanna3@users.noreply.github.com>
2022-12-05 12:58:17 +0100

    Fixed some miner issuses//Anna

--- src/CarTransporter.java
+++ src/CarTransporter.java
@@ -1,78 +1,73 @@
 import java.awt.Color;
 import java.util.ArrayList;
 
-public class CarTransporter extends Truck{
+public class CarTransporter extends Truck {
     private ArrayList<Car> loadcarslist;
-    
-    int transporterx =getxPos();//should get pos from Truck?
-    int transportery =getyPos();
 
+    int transporterx = getxPos();// should get pos from Truck?
+    int transportery = getyPos();
 
     public CarTransporter() {
         super(2, 110, 0, Color.BLUE, "car transporter", 0, 0, Direction.NORTH);
     }
-    
-//det ska vara max och min i platform; 
 
+    // det ska vara max och min i platform;
 
-@Override
-public void openPlatform(){ 
-      //platform is not in the superclass, cant override
-    platform.setPlatformAngle(platform.maxAngle);                         //can we move the open and close func to Truck?
-}
-
-@Override
-public void closePlatform(){       //platform is not in the superclass, cant override
-    platform.setPlatformAngle(platform.minAngle);                          //can we move the open and close func to Truck?
-}
-
-
-
-private boolean Carxposisclose(Car car, int transporterx){  //check nearby cars in x
-    if(car.getxPos() < (transporterx+20) & car.getxPos() > (transporterx-20)){
-        return true;
-    }else{
-        return false;
+    @Override
+    public void openPlatform() {
+        // platform is not in the superclass, cant override
+        platform.setPlatformAngle(platform.maxAngle); // can we move the open and close func to Truck?
     }
-}
 
-private boolean Caryposisclose(Car car, int transportery){  //check nearby cars in y
-    if(car.getyPos() < (transportery+20) & car.getyPos() > (transportery-20)){
-        return true;
-    }else{
-        return false;
+    @Override
+    public void closePlatform() { // platform is not in the superclass, cant override
+        platform.setPlatformAngle(platform.minAngle); // can we move the open and close func to Truck?
     }
-}
 
-public void findCloseCars(int transporterx, int transportery){
-
-    loadcarslist = new ArrayList<Car>();        //make a list of nearby cars
-    
-    for (Car car : Main.cars){
-        if(Carxposisclose(car, transportery) & Caryposisclose(car, transportery)){
-            loadcarslist.add(car);
+    private boolean Carxposisclose(Car car, int transporterx) { // check nearby cars in x
+        if (car.getxPos() < (transporterx + 20) & car.getxPos() > (transporterx - 20)) {
+            return true;
+        } else {
+            return false;
         }
     }
-}
 
-//do we need a loadCars function? Yes!/HC
-
-public void loadCars(){
-    platform.openPlatform();
-    if platformAngle>0
-      
-    }
-
-
-
-
-public void unloadCars(){
-    if(platform.platformAngle == 70){
-        for (Car car : loadcarslist) { //unload = yeet from list
-            car.setxPos(transporterx-10);   //unload next to the transporter // WANT to unload into carshop
-            car.setyPos(transportery);
+    private boolean Caryposisclose(Car car, int transportery) { // check nearby cars in y
+        if (car.getyPos() < (transportery + 20) & car.getyPos() > (transportery - 20)) {
+            return true;
+        } else {
+            return false;
         }
     }
-}
+
+    public void findCloseCars(int transporterx, int transportery) {
+
+        loadcarslist = new ArrayList<Car>(); // make a list of nearby cars
+
+        for (Car car : Main.cars) {
+            if (Carxposisclose(car, transportery) & Caryposisclose(car, transportery)) {
+                loadcarslist.add(car);
+            }
+        }
+    }
+
+    // do we need a loadCars function? Yes!/HC
+
+    public void loadCars() {
+        platform.openPlatform();
+        if (platform.getPlatformAngle() == platform.maxAngle) {
+                //TODO FIX THIS Hälsningar Anna
+        }
+
+    }
+
+    public void unloadCars() {
+        if (platform.getPlatformAngle() == platform.maxAngle) {
+            for (Car car : loadcarslist) { // unload = yeet from list
+                car.setxPos(transporterx - 10); // unload next to the transporter // WANT to unload into carshop
+                car.setyPos(transportery);
+            }
+        }
+    }
 
 }

9008eb2fd1dc203e3c258495bd86964f5c32a869
Melisa730 <117914333+Melisa730@users.noreply.github.com>
2022-12-05 10:39:58 +0100

    Testing, ändrade lite i platform, truck och scania

--- src/Scania.java
+++ src/Scania.java
@@ -1,12 +1,40 @@
 import java.awt.*;
-
-public class Scania extends Truck {
-
+public class Scania extends Car {
+public double platformAngle;
+Turbo t  = new Turbo();
+Platform platform;
 public Scania(){
 super(2, 162, 0 ,Color.white, "Scania Dump Truck", 0, 0, Direction.NORTH);
 //turboOn = false;
-//peedFactor();
+speedFactor();
 }
-
-
+// om angle > 0 -> kan inte öka speed
+// om speed > 0 -> kan inte raise eller lower
+// vi får ha speed = 0 och angle == 0
+public void raiseplatform(double platformAngle) throws Exception{
+    if (getCurrentSpeed()==0){  //platform.getPlatformAngle()> 0 &
+        platform.openPlatform(platformAngle);                             //platform.closePlatform, intervallet hanteras av openplatform
+    }else if(getCurrentSpeed() > 0){ //getPlatformAngle() == 0  (?)
+        throw new Exception("raising the platform is not possible");
+    }
+}
+public void lowerplatform(double platformAngle) throws Exception{
+    if (getCurrentSpeed()==0){  //platform.getPlatformAngle()< 70 &
+        platform.closePlatform(platformAngle);  //close not open
+    }else if(getCurrentSpeed() > 0){  //getPlatformAngle() == 70 ||
+        throw new Exception("lowering the platform is not possible");
+    }
+}
+public void setTurboOn(){
+        t.setTurboOn();
+    }
+    public void setTurboOff(){
+        t.setTurboOff();
+    }
+    protected double speedFactor(){
+        return getEnginePower() * 0.01 * t.turbofactor();
+    }
+    public double getPlatformAngle() {
+        return platformAngle;
+    }
 }

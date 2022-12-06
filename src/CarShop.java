f6df43632a9ae64de64547bebdc0acb4455e2fc4
hannapanna3 <117651241+hannapanna3@users.noreply.github.com>
2022-12-06 12:53:03 +0100

    Worked on Load/Unload.

--- src/CarShop.java
+++ src/CarShop.java
@@ -2,46 +2,65 @@
 Cars can only be loaded if they are reasonably close to the workshop.
 When a car is unloaded, it should then end up reasonably close to the workshop. */
 
-
 //NEEDS: position, Load(recieve cars from CarTransporter)- and Unload(Unload next to shop)- function
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public class CarShop  {
-    private double x; //X position for carshop
-    private double y; //Y position for carshop
-    private double radius; //Raduius for in what distance the cars can go to this shop
+public class CarShop {
+    private int x; // X position for carshop
+    private int y; // Y position for carshop
+    private double radius; // Raduius to check if cars are reasonably close
     private int carAmount; // How many cars that can max be in shop
-    private ArrayList<Car> carsInShop;
+    private ArrayList<Car> carsInShop; //List if how many cars are in the shop
 
-    public CarShop(double x, double y, double radius, int carAmount){
-        
-        this.x = x; //Position
-        this.y = y; //Position
-        this.radius = radius; //check if cars are reasonably close 
-        this.carAmount = carAmount; //Max cars allowed
+    public CarShop(int x, int y, double radius, int carAmount) {
+
+        this.x = x; 
+        this.y = y; 
+        this.radius = radius; 
+        this.carAmount = carAmount; 
     }
-    
-    private boolean howManyCarsInShop(){ //när vi skapar LOAD, kolla "howManyCarsInShop" först, beroende på den kan vi loada eller ej
 
-        if(carsInShop.size() >= carAmount){
+    public int getxPos() {
+        return x;
+    }
+
+    public int getyPos() {
+        return y;
+    }
+   
+    public double getRadius(){ //FIX RADIUS STUFF,needs function for calculating distance between car and CarShop
+        return radius;
+    }
+
+    public void carAmount(){
+        carAmount = 2;
+    }
+
+
+    private boolean howManyCarsInShop() { 
+        if (carsInShop.size() >= carAmount) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
-    
 
-    
-    public void unloadCars(CarTransporter carTransporter){
-        carTransporter.unloadCars();
+    public void loadCars(Car car){
+        
+        if(howManyCarsInShop() == true){
+            carsInShop.add(car);
+
+            //add radius stuff
+        }
     }
+
+    public void unloadCars(Car car) {
+        //add radius stuff
+    }
+
     
 }
-
-
-
-
 
 

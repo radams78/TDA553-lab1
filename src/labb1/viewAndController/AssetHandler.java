17d8d54a330f5a3edb36c73cd6d13faf4f97c4c7
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-12-08 15:35:42 +0100

    added some comments and injection safety for asset handler

--- src/labb1/viewAndController/AssetHandler.java
+++ src/labb1/viewAndController/AssetHandler.java
@@ -10,26 +10,73 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class AssetHandler {
-    private HashMap<Point, BufferedImage> assetPointMap = new HashMap<Point, BufferedImage>();
-    private HashMap<String, BufferedImage> assetNameMap = new HashMap<String, BufferedImage>();
+    private HashMap<Point, BufferedImage> assetPointMap = new HashMap<Point, BufferedImage>(); // A HashMap that keeps
+                                                                                               // track of what images
+                                                                                               // are at which points
+    private HashMap<String, BufferedImage> assetNameMap = new HashMap<String, BufferedImage>(); // A HashMap that keeps
+                                                                                                // track of what names
+                                                                                                // are connected to what
+                                                                                                // images(for cpu
+                                                                                                // offloading)
 
+    /**
+     * addNamedImageToDict
+     * 
+     * This function maps string to particular images and stores it in a hash map
+     * 
+     * @param name The name of the image (Has to match or exeption is thrown). Name also needs to be 10 chars or under for injection safety
+     * 
+     */
     public void addNamedImageToDict(String name) {
-        BufferedImage image = makeImageFromModelName(name);
-        assetNameMap.put(name, image);
+        if (name.length()<=10){
+            BufferedImage image = makeImageFromModelName(name);
+            assetNameMap.put(name, image);
+        }else{
+            throw new IllegalArgumentException("String was " + name.length() + " long when its supposed to be under 10 chars");
+        }
+        
     }
 
+    /**
+     * findImageFromName
+     * 
+     * This function finds an image from a string name in the given image directory
+     * 
+     * @param name The name of the image (Has to match or exeption is thrown)
+     * 
+     */
+
+    // TODO make this injection safe
     private BufferedImage findImageFromName(String name) {
         if (assetNameMap.containsKey(name)) {
             return assetNameMap.get(name);
-        }else   {
-            throw new IllegalArgumentException("Name "+ name + " doesnt exist");
+        } else {
+            throw new IllegalArgumentException("Name " + name + " doesnt exist");
         }
     }
 
+    /**bindPointToNamedImage
+     * 
+     * This function binds a point to an image from a given name. Name has to exist in prior dict or throws exception
+     * 
+     * @param modelName the name to search for
+     * 
+     * @param point any point that is to be linked to the image
+     * 
+     */
     public void bindPointToNamedImage(String modelName, Point point) {
         assetPointMap.put(point, findImageFromName(modelName));
     }
 
+    /**makeImageFromModelNam
+     * 
+     * this function makes an image using the Buffered Image library from a given place
+     * 
+     * @param modelName this needs to exist as an image name(case sensitive) or exception is thrown
+     * 
+     */
+
+     //TODO make this injection safe
     private BufferedImage makeImageFromModelName(String modelName) {
         try {
             BufferedImage image = ImageIO
@@ -43,19 +90,15 @@ public class AssetHandler {
 
     public BufferedImage getAssetFromPoint(Point point) {
         if (assetPointMap.containsKey(point)) {
-            //TODO see if you can be more defensive here
+            // TODO see if you can be more defensive here
             return assetPointMap.get(point);
-        }else{
-            throw new IllegalArgumentException("point " + point +" does not exist in hash");
+        } else {
+            throw new IllegalArgumentException("point " + point + " does not exist in hash");
         }
     }
 
-
-    public void refresh(){
+    public void refresh() {
         assetPointMap = new HashMap<Point, BufferedImage>();
     }
-
-
-
 
 }

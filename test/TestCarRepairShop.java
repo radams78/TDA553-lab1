0a21f926da13f0e3d501c59653f97df9a70d8b4c
Snebabe <113423765+Snebabe@users.noreply.github.com>
2022-12-08 15:49:28 +0100

    Update TestCarRepairShop.java

--- test/TestCarRepairShop.java
+++ test/TestCarRepairShop.java
@@ -1,1 +1,29 @@
+package test;
 
+import src.State;
+import src.CarRepairShop;
+import src.Saab95;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class TestCarRepairShop {
+    @Test
+    public void demoTestMethod() {
+        assertTrue(true);
+    }    
+
+    CarRepairShop repairShop;
+     
+    @BeforeEach
+    public void reset() {
+        repairShop = new CarRepairShop();
+    }
+
+    @Test
+    public void testNewCar() {
+        Car car = new Saab95()
+        repairShop.add(car)
+        assertEquals(1, repairShop.getLoadedCars().getAmount())
+}

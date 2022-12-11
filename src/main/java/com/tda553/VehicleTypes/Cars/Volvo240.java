216bdfeb7836d9fe8d59d875edff6cfe7f011175
User <email@example.com>
2022-12-11 15:19:49 +0100

    Fix #1

--- src/main/java/com/tda553/VehicleTypes/Cars/Volvo240.java
+++ src/main/java/com/tda553/VehicleTypes/Cars/Volvo240.java
@@ -1,12 +1,8 @@
 package com.tda553.VehicleTypes.Cars;
 import java.awt.Color;
 
-import com.tda553.Models.Vehicle;
-
-public class Volvo240 extends Vehicle
+public class Volvo240 extends GenericCarWithTrim
 {
-    private final static double trimFactor = 1.25;
-
     public Volvo240()
     {
         setNrDoors(4);
@@ -14,11 +10,7 @@ public class Volvo240 extends Vehicle
         setEnginePower(100);
         setModelName("Volvo240");
         setVehicleWeight(1500);
+        setTrimFactor(1.25);
         stopEngine();
-    }
-
-    public double speedFactor()
-    {
-        return getEnginePower() * 0.01 * trimFactor;
     }
 }

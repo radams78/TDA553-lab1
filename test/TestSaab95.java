9509982b6d83bfa4aa0b405446475740177654ac
Snebabe <eliassoderberg03@gmail.com>
2022-12-12 14:41:50 +0100

    Co-authored-by: DuchessMuhr <DuchessMuhr@users.noreply.github.com> Co-authored-by: Adam Kvarnsund <adamkvarnsund@users.noreply.github.com>

---
+++ test/TestSaab95.java
@@ -0,0 +1,62 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import Model.Direction;
+import Model.Saab95;
+
+public class TestSaab95 {
+
+    @Test
+    public void demoTestMethod() {
+        assertTrue(true);
+    }    
+
+    Saab95 car;
+     
+    @BeforeEach
+    public void reset() {
+        car = new Saab95();
+    }
+
+    @Test
+    public void testStartEngine() {
+        car.startEngine();
+        assertEquals(0.1, car.getCurrentSpeed());
+    }
+
+    @Test
+    public void testStopEngine() {
+        car.stopEngine();
+        assertEquals(0, car.getCurrentSpeed());
+    }
+
+    @Test
+    public void testTurnLeft() {
+        car.setDirection(Direction.RIGHT);
+        car.turnLeft();
+        assertEquals(Direction.UP, car.getDirection());
+        car.turnLeft();
+        car.turnLeft();
+        assertEquals(Direction.DOWN, car.getDirection());
+    }
+
+    @Test
+    public void testTurnRight() {
+        car.setDirection(Direction.RIGHT);
+        car.turnRight();
+        assertEquals(Direction.DOWN, car.getDirection());
+        car.turnRight();
+        car.turnRight();
+        assertEquals(Direction.UP, car.getDirection());
+    }
+
+    @Test
+    public void testValidGasInput() {
+        // assertThrows(IllegalArgumentException.class, car.gas(20));
+        // BEHÖVER FRÅGA TA
+    }
+
+}
---
+++ test/TestVehicle.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import Model.Direction;
+import Model.*;
+
+public class TestVehicle {
+    
+
+    Vehicle vehicle;
+
+    @BeforeEach
+    public void reset() {
+        vehicle = new Volvo240();
+    }
+
+    @Test   
+    public void testMove(){
+        vehicle.setCurrentSpeed(5);
+        vehicle.move();
+        assertEquals(5, vehicle.getX());
+    }
+
+    @Test   
+    public void gas() {
+        vehicle.gas(1);
+        
+    }
+    
+    @Test
+    public void brake(){
+        vehicle.brake(1)';'
+    }
+
+        
+}
